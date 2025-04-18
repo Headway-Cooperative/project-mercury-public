4f4e4d3de47501f95e40719d299326c25d25230e
Andrew Freitas <andrewfreitas09@gmail.com>
2025-04-18 13:45:50 -0600

    remove condition on jobuser

--- gcp/main.tf
+++ gcp/main.tf
@@ -12,9 +12,12 @@ provider "google" {
 }
 
 variable "projectId" {
+  default = "crested-trainer-457019-h6"
 }
 
 variable "datasetName" {
+  # default = "mercury_billing_data.gcp_billing_export_resource_v1_0185D8_D9CD43_0B6637"
+  default = "mercury_billing_data"
 }
 
 resource "google_service_account" "account" {
@@ -36,8 +39,4 @@ resource "google_project_iam_member" "bigquery_job_user" {
   project = var.projectId
   role    = "roles/bigquery.jobUser"
   member  = "serviceAccount:mercury-accessor-sa@mercuryaccessor.iam.gserviceaccount.com"
-  condition {
-    title      = "only billing access"
-    expression = "resource.name == 'projects/${var.projectId}/datasets/${var.datasetName}'"
-  }
 }
